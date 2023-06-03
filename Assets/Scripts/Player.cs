1e0723f4e854286da922f875a74c7ec57b456400
Qwis <118746220+Qwisss@users.noreply.github.com>
2023-06-03 20:50:23 +0300

    1.4
    BACK TO HELL FUCKING ANIMATIONS!!!!!

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,10 +7,38 @@ public class Player : MonoBehaviour
     [SerializeField] private float _speed;
     [SerializeField] private CharacterController _characterController;
 
+    Animator animator;
 
+    Vector3 targetPosition;
+    Vector3 startGamePosition;
+    Quaternion startGameRotation;
+   
 
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        startGamePosition = transform.position;
+        startGameRotation = transform.rotation;
+        targetPosition = transform.position;
+
+    }
     private void Update()
     {
         _characterController.Move(Vector3.right * _speed * Input.GetAxis("Horizontal") * Time.deltaTime);
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            animator.SetTrigger("Jump");
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            animator.SetTrigger("Slide");
+        }
+    }
+
+    public void StartRun()
+    {
+        animator.SetTrigger("Run");
+        
     }
 }
